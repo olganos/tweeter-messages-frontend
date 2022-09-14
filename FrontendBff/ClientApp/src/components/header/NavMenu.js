0941775b4e6452e4d53e51d409a420c0cca6c7f7
Olga Nosova <semverst@yandex.ru>
2022-09-14 10:02:52 +0200

    added search input and a button

--- FrontendBff/ClientApp/src/components/header/NavMenu.js
+++ FrontendBff/ClientApp/src/components/header/NavMenu.js
@@ -1,5 +1,5 @@
 import { NavLink, Link } from 'react-router-dom';
-import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, Input, InputGroup, Button } from 'reactstrap';
 import styles from './NavMenu.module.scss';
 import UserInfo from './UserInfo';
 
@@ -19,6 +19,18 @@ export default function Header() {
     <header>
       <Navbar className={`border-bottom ${styles.boxShadow} mb-3`} container light>
         <NavbarBrand tag={Link} to="/">Tweeter</NavbarBrand>
+        <InputGroup style={{ "width": "250px" }} size="sm">
+          <Input
+            placeholder="user name"
+          />
+          <Button
+            color="secondary"
+            outline
+            onClick={(e) => console.log(e)}
+          >
+            Search
+          </Button>
+        </InputGroup>
         <Nav className="d-flex flex-row">
           {/* on the main page show user name, user's tweets and a form for a new tweet */}
           {/* if not athenticated, redirect to identity server */}
@@ -33,7 +45,7 @@ export default function Header() {
           </NavItem>
           <NavItem>
             <NavLink
-              to="/all-tweets" //todo: Tweets (all, but rectric somehow - by pages of load dinamically)
+              to="/all-tweets" //todo: Tweets (all, but rectrict somehow - by pages of load dinamically)
               style={getNavStyle}
               end
             >
